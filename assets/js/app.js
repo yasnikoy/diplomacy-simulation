9c120b1b3bfc458c8b0ed60d0dd61abe48a6d139
yasnikoy <yasnikoy@hotmail.com>
2025-12-25 21:30:38 +0300

    Fix: Remove unused phoenix-colocated import causing build error

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,7 +22,6 @@ import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
-import {hooks as colocatedHooks} from "phoenix-colocated/diplomacy"
 import topbar from "../vendor/topbar"
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
